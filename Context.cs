d6404c2613d66fafc27ec6ee8cd37228a2898b3b
matheus-proenca <matheusproenca2020@hotmail.com>
2025-07-05 09:29:01 -0300

    Config context finish

---
+++ Context.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TechStoreApi.models;
+
+namespace TechStoreApi
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+        public DbSet<Produto> Produto { get; set; } = null!;
+    }
+}
